7dd35a55c017acec8162499a13a6aca389c9656a
Ninad3721 <ninadtrivedi25@gmail.com>
2023-03-21 22:02:46 +0530

    ProjectCard added

--- frontend/src/pages/Funderlogin.js
+++ frontend/src/pages/Funderlogin.js
@@ -7,35 +7,47 @@ import { createClient } from '@supabase/supabase-js'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import Funder_account from './Funder_account'
 import ProjectCard from '@/components/ProjectCard'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { ConnectButton } from 'web3uikit'
+
+
 
 export default function FunderLogin() {
+  const [cardData, setCardData] = useState([])
   const session = useSession()
   const supabase = useSupabaseClient()
   const getData = async () => {
     const { data, error } = await supabase
       .from('Projects')
       .select()
-    console.log(data)
+    setCardData(data)
   }
 
   const load = useEffect(() => {
-
     getData()
   }, [])
 
   return (
+    <>
+      <ConnectButton />
+      <div className="container" style={{ padding: '50px 0 100px 0' }}>
+        <p>Funder Login</p>
+        {!session ? (
+          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
+        ) : (
+          <>
+            <Funder_account session={session} />
+            {
+              cardData.map(() => {
+                return <ProjectCard />
+              })
 
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      <p>Funder Login</p>
-      {!session ? (
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
-      ) : (
-        <>
-          <Funder_account session={session} />
-          <ProjectCard></ProjectCard>
-        </>
-      )}
-    </div>
+
+            }
+          </>
+        )}
+      </div>
+    </>
+
   )
 }
