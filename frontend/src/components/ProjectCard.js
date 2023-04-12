5b91ee3c1759b9e8658280d79c16f9e064c3f079
Ninad3721 <ninadtrivedi25@gmail.com>
2023-04-12 22:51:31 +0530

    Transaction fullRetrieve changed

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -27,22 +27,27 @@ export default function ProjectCard(props) {
     const [expanded, setExpanded] = React.useState(false);
     const [funding, setFuding] = React.useState(0)
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-
     const signer = provider.getSigner();
+    const transaction_contract = new ethers.Contract("0xcd9D19922E215B8c8C78B5aF531B92a775e7363a", abi, signer)
 
-    const fetchLatestData = async () => {
+    const fetchLatestProjectData = async () => {
         const { data, error } = await supabase.from("project_profile").select('*').eq('project_id', props.project_id)
         console.log(data[0].project_id)
         return data[0];
     }
+    const fetchLatestFunderToProjectData = async () => {
+        const account = await provider.send("eth_requestAccounts", [])
+        const { data, error } = await supabase.from("funderToProject").select('*').eq('funder_address', account)
+        return data;
+    }
 
-    const UpdateFunder = async () => {
-        const readData = await fetchLatestData();
+
+    const UpdateInventor = async () => {
+        const readData = await fetchLatestProjectData();
         const { data, error } = await supabase
             .from('project_profile')
             .update({ numberOfFunder: Number(readData.numberOfFunder) + 1, fundCollected: Number(readData.fundCollected) + Number(funding) })
             .match({ project_id: readData.project_id });
-
         console.log(error)
         console.log(Number(props.fundCollected) + Number(funding))
 
@@ -50,23 +55,29 @@ export default function ProjectCard(props) {
 
     const insertFunder = async () => {
         const account = await provider.send("eth_requestAccounts", [])
-        const { data, error } = await supabase.from("funderToProject").insert({
-            funder_address: account[0],
-            project_id: props.project_id,
-            amt_funded: funding,
-        })
-        console.log("hello")
-        console.log(error)
+        const fetchData = await fetchLatestFunderToProjectData()
+        if (fetchData.length == null) {
+            const { data, error } = await supabase.from("funderToProject").insert({
+                funder_address: account[0],
+                project_id: props.project_id,
+                amt_funded: funding,
+            })
+        }
+        else {
+            const { data, error } = await supabase
+                .from('funderToProject')
+                .update({ amt_funded: fetchData[0].amt_funded + Number(funding) })
+                .match({ funder_address: account, project_id: props.project_id });
+        }
 
     }
 
     const handleDonate = async () => {
 
-
         if (funding == 0) {
             window.alert("Please enter funding value")
         }
-        const transaction_contract = new ethers.Contract("0xcd9D19922E215B8c8C78B5aF531B92a775e7363a", abi, signer)
+
         const options = {
             value: ethers.utils.parseEther(funding.toString())
         }
@@ -74,9 +85,12 @@ export default function ProjectCard(props) {
         window.alert("Transaction Succesful")
         console.log("hola")
         await insertFunder()
-        await UpdateFunder()
+        await UpdateInventor()
     }
 
+    const handleRetrieve = async () => {
+        const retrieve = await transaction_contract.retrieve()
+    }
 
 
     return (
@@ -119,8 +133,7 @@ export default function ProjectCard(props) {
                     </IconButton>
                     <input type="number" onChange={(e) => { setFuding(e.target.value) }}></input>
                 </CardActions>
-                <Button style={{ visibility: `${(props.full_refund_eligible ? "visible" : "visible")}` }}>Retrieve</Button>
-
+                <Button style={{ visibility: `${(props.full_refund_eligible ? "visible" : "visible")}` }} onClick={handleRetrieve}>Retrieve</Button>
 
             </Card>
         </>
